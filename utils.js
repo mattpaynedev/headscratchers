b742c348bea23ccbd4ff9a81be8e0d91b1454e15
mattpaynedev <72278561+mattpaynedev@users.noreply.github.com>
2022-01-19 21:51:44 -0800

    Fix words.js file - in wrong directory

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-import { words5Letter } from "./pages/words";
+import { words5Letter } from "./words";
 
 export function chooseWord() {
     let idx = Math.floor(Math.random() * words5Letter.length)
